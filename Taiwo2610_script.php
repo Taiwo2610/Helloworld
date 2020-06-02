5ce3b6722789e8d9319dcde86f13e641e8643c62
Taiwo Agunbiade <66220037+Taiwo2610@users.noreply.github.com>
2020-06-03 00:44:35 +0100

    Taiwo2610_script.php

---
+++ Taiwo2610_script.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+      <?php
+          $characterName = "Taiwo Agunbiade";
+          echo "<h1>Hello World, this is $characterName with HNGi7 ID:HNG-02166 using php language for stage 2 task. </h1>";
+
+       ?>
+
+
+  </body>
+</html>
